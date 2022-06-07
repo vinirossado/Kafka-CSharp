71f40a7fa4c7d866970c6645b6a10dc1a6d81383
Vinicius Rossado <vinirossado@gmail.com>
2022-06-07 11:58:14 +0300

    Initial Implementation

---
+++ KafkaProducerConsumer/KafkaConsumer/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ConsumerConfig
+{
+    GroupId = "weather-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("weather-topic");
+
+CancellationTokenSource token = new();
+
+try
+{
+    while (true)
+    {
+        var response = consumer.Consume(token.Token);
+        if (response.Message != null)
+        {
+            var weather = JsonConvert.DeserializeObject<Weather>
+                (response.Message.Value);
+
+            Console.WriteLine($"State: {weather.State}, " +
+                $"Tem: {weather.Temperature}C");
+        }
+    }
+}
+catch (Exception)
+{
+
+    throw;
+}
+
+public record Weather(string State, int Temperature);
---
+++ KafkaProducerConsumer/KafkaProducer/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    string? state;
+    while ((state = Console.ReadLine()) != null)
+    {
+
+        var response = await producer.ProduceAsync("weather-topic",
+            new Message<Null, string>
+            {
+                Value = JsonConvert.SerializeObject(
+                new Weather(state, 70))
+            });
+        Console.WriteLine(response.Value);
+    }
+
+}
+catch (ProduceException<Null, string> ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+public record Weather(string state, int Temperature);
